a5d3d8fdb1aae09d7bc4656351b2973a5448b474
Ayesha14786 <ayesa.farsana@lmnas.com>
2024-08-01 16:06:17 +0000

    the emali is skipped but has internal error in terminal

--- pdf_on_submit/__init__.py
+++ pdf_on_submit/__init__.py
@@ -1,1 +1,5 @@
 __version__ = "15.0.1"
+from frappe.workflow.doctype.workflow_action import workflow_action
+from pdf_on_submit.monkey_patch import custom_send_workflow_action_email
+
+workflow_action.send_workflow_action_email = custom_send_workflow_action_email
