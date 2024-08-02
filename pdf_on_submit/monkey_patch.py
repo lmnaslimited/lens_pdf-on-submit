4ea1f7c1ae9bcfa30fd5287689e22aee7beef28a
Ayesha14786 <ayesa.farsana@lmnas.com>
2024-08-02 05:28:25 +0000

    changed the function

--- pdf_on_submit/monkey_patch.py
+++ pdf_on_submit/monkey_patch.py
@@ -1,82 +1,51 @@
-# from typing import TYPE_CHECKING, Union
-# import frappe
-
-# if TYPE_CHECKING:
-#     from frappe.model.document import Document
-#     from frappe.workflow.doctype.workflow.workflow import Workflow
-
-# @frappe.whitelist()
-# def custom_get_transitions(
-#     doc: Union["Document", str, dict], workflow: "Workflow" = None, raise_exception: bool = False
-# )-> list[dict]:
-    
-#     transitions = frappe.call("frappe.model.workflow.get_transitions", doc = doc, workflow = workflow)
-
-#     # custom_transition_method = frappe.call(workflow.custom_transition_method, get_transition)
-#     return transitions
-
 import frappe
 from frappe import _
-from frappe.desk.notifications import clear_doctype_notifications
 from frappe.model.workflow import (
 	get_workflow_name,
-	send_email_alert,
 )
-from frappe.utils.background_jobs import enqueue
 from frappe.workflow.doctype.workflow_action.workflow_action import (
-	create_workflow_actions_for_roles,
-	clear_workflow_actions,
-	is_workflow_action_already_created,
-	update_completed_workflow_actions,
 	get_doc_workflow_state,
-	get_next_possible_transitions,
-	send_workflow_action_email,
+	get_users_next_action_data
 )
 
+'''
+This function is equivalent to the standard "get_users_next_action_data"
+located at "frappe.workflow.doctype.workflow_action.workflow_action".
 
-def custom_send_workflow_action_email(doc, transitions):
-	# workflow = get_workflow_name(doc.get("doctype"))
-	# if not workflow:
-	# 	return
+This customization is known as monkey patching in ERPNext. To apply this custom function,
+we need to specify the path to this function in the "__init__.py" file of the custom app.
 
-	# if state == "on_trash":
-	# 	clear_workflow_actions(doc.get("doctype"), doc.get("name"))
-	# 	return
+The "__init__.py" file should be located at:
+"apps/custom_app/custom_app/__init__.py".
 
-	# if is_workflow_action_already_created(doc):
-	# 	return
+By doing this, we override the original function with our custom implementation.
+'''
 
-	# update_completed_workflow_actions(doc, workflow=workflow, workflow_state=get_doc_workflow_state(doc))
-	# clear_doctype_notifications("Workflow Action")
+def custom_get_users_next_action_data(transitions, doc):
 
-	# next_possible_transitions = get_next_possible_transitions(workflow, get_doc_workflow_state(doc), doc)
+	user_data_map = {}
 
-	# if not next_possible_transitions:
-	# 	return
+	#getting the workflow name from the doctype
+	workflow = get_workflow_name(doc.get("doctype"))
 
-	# roles = {t.allowed for t in next_possible_transitions}
-	# create_workflow_actions_for_roles(roles, doc)
+	if not workflow:
+		return
+	
+	'''
+	Check if the state should mute email notifications.
+	To achieve this, we need to customize the "Workflow Document State" doctype
+	by adding a field named "custom_mute_email_notification" which is a checkbox.
 
-	# mute_email = frappe.get_cached_value("Workflow Document State", {"parent": workflow, "state": get_doc_workflow_state(doc)}, "custom_mute_email_notification")
-    
-	# if send_email_alert(workflow) and not mute_email:
-	# 	enqueue(
-	# 		send_workflow_action_email,
-	# 		queue="short",
-	# 		doc=doc,
-	# 		transitions=next_possible_transitions,
-	# 		enqueue_after_commit=True,
-	# 	)
-	# # frappe.msgprint("this is to test the monkey patch")
+	'''
+	mute_email = frappe.get_cached_value(
+		"Workflow Document State", 
+		{"parent": workflow, "state": get_doc_workflow_state(doc)}, 
+		"custom_mute_email_notification"
+		)
+	
+	if not mute_email:
+		return get_users_next_action_data(transitions, doc)
+	
+	return user_data_map
 
-    workflow = get_workflow_name(doc.get("doctype"))
-    if not workflow:
-        return
-    mute_email = frappe.get_cached_value("Workflow Document State", {"parent": workflow, "state": get_doc_workflow_state(doc)}, "custom_mute_email_notification")
-    
-    if (not mute_email):
-        return send_workflow_action_email(doc, transitions)
-    else: 
-        frappe.msgprint("don't trigger the email")
-        return []
     
