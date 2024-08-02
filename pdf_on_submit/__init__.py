0665b24591aaf2a688c6d1b9453e962c1bd6bda0
Ayesha14786 <ayesa.farsana@lmnas.com>
2024-08-02 05:28:25 +0000

    changed the function

--- pdf_on_submit/__init__.py
+++ pdf_on_submit/__init__.py
@@ -1,5 +1,9 @@
 __version__ = "15.0.1"
-from frappe.workflow.doctype.workflow_action import workflow_action
-from pdf_on_submit.monkey_patch import custom_send_workflow_action_email
 
-workflow_action.send_workflow_action_email = custom_send_workflow_action_email
+'''
+Monkey Patch Technique
+'''
+from frappe.workflow.doctype.workflow_action import workflow_action
+from pdf_on_submit.monkey_patch import custom_get_users_next_action_data
+
+workflow_action.get_users_next_action_data = custom_get_users_next_action_data
